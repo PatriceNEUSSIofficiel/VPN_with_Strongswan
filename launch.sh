08e080e92ddb122ea4b3b55ef462b82b0658b67d
patriceNEUSSIofficiel <patriceneussi9@gmail.com>
2024-03-13 21:51:08 +0100

    v3

--- launch.sh
+++ launch.sh
@@ -9,7 +9,7 @@ sudo setfacl --modify user:${USER}:rw /var/run/docker.sock
 cd /home/patrice/Desktop/INFOL3/admin/reseau/TP/strongswan
 
 # Lancement du conteneur Ubuntu 1
-docker run -tid --privileged --name ubuntu1-container -v /var/run/docker.sock:/var/run/docker.sock ubuntu1-image sleep infinity
+docker run -tid --privileged  --network ubuntu_net --name ubuntu1-container -v /var/run/docker.sock:/var/run/docker.sock ubuntu1-image sleep infinity
 
 # Attente pour permettre au conteneur Ubuntu de démarrer complètement
 sleep 5
@@ -25,7 +25,7 @@ docker exec -i ubuntu1-container bash -c 'docker exec -itd centos2 yum install -
 docker exec -i ubuntu1-container bash -c 'docker exec -itd centos3 yum install -y iputils-ping'
 
 # Lancement du conteneur Ubuntu 2
-docker run -tid --privileged --name ubuntu2-container -v /var/run/docker.sock:/var/run/docker.sock ubuntu2-image sleep infinity
+docker run -tid --privileged --network ubuntu_net2 --name ubuntu2-container -v /var/run/docker.sock:/var/run/docker.sock ubuntu2-image sleep infinity
 
 # Attente pour permettre au conteneur Ubuntu de démarrer complètement
 sleep 5
@@ -41,14 +41,14 @@ docker exec -i ubuntu2-container bash -c 'docker exec -itd centos5 yum install -
 docker exec -i ubuntu2-container bash -c 'docker exec -itd centos6 yum install -y iputils-ping'
 
 # Affichage des adresses IP des conteneurs et du réseau
-eho "centos-container(1,2,3,4,5,6) ips : "
+echo "centos-container(1,2,3,4,5,6) ips : "
 docker exec -i ubuntu1-container bash -c 'docker network inspect ubuntu_net | grep IPAddress'
 docker exec -i ubuntu1-container bash -c 'docker inspect --format="{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $(docker ps -aq)'
 #ubuntu_net "Subnet": "172.24.0.0/16" : centos3 - 172.24.0.4 centos2 - 172.24.0.3 centos1 - 172.24.0.2 
 #ubuntu_net2 "Subnet": "172.25.0.0/16" : centos6 - 172.25.0.4 centos5 - 172.25.0.3 centos4 - 172.25.0.2 
-eho "ubuntu1-container ip : "
+echo "ubuntu1-container ip : "
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ubuntu1-container
 #ubuntu1-container : 172.17.0.2 "Subnet": "172.17.0.0/16",
-eho "ubuntu2-container ip : "
+echo "ubuntu2-container ip : "
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ubuntu2-container
 #ubuntu2-container : 172.17.0.3 "Subnet": "172.17.0.0/16",
